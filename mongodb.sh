3ec11cfacb4bf5bdecb2db904a9d653d1ec3fc8f
valli25 <pravallika.tsm@gmail.com>
2024-09-04 14:27:27 -0700

    adding files

--- mongodb.sh
+++ mongodb.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
-ID=(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%s)
+
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
-    if [ $? -ne 0 ]
-then 
-   echo  - e "ERROR:: $2 ... $R failed $N"
-   exit 1
-else 
-  echo -e "$2 ... $G success $N"
-fi
-} 
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
 if [ $ID -ne 0 ]
 then
-  echo -e "$R ERROR:: please run this script with root access $N"
-  exit 1
-else  
-  echo "you are root user"
-fi 
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+    
+fi # fi means reverse of if, indicating condition end
 
-cp /home/ec2-user/Roboshop-Shellscript/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
 
-yum install mongodb-org -y &>> $LOGFILE
+dnf install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
 
@@ -46,3 +53,4 @@ VALIDATE $? "Remote access to MongoDB"
 systemctl restart mongod &>> $LOGFILE
 
 VALIDATE $? "Restarting MongoDB"
+
