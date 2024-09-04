faaceb675bd6775a1db71124ba9d87817377a45f
valli25 <pravallika.tsm@gmail.com>
2024-09-04 14:52:40 -0700

    adding files

--- user.sh
+++ user.sh
@@ -69,7 +69,7 @@ npm install  &>> $LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
-cp /home/ec2-user/Roboshop-Shellscript/user.service /etc/systemd/system/user.service
+sudo cp /home/ec2-user/Roboshop-Shellscript/user.service /etc/systemd/system/user.service
 
 VALIDATE $? "Copying user service file"
 
